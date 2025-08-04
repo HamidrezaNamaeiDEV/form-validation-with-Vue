c371383c57469b7ef458034390e0f46e6f5f6cd1
hamidreza.namaeimoghadam <hamidreza.namaeimoghadam@partdp.ir>
2025-08-04 18:05:03 +0330

    test validation with yup and yup schema

--- src/components/LoginForm1.vue
+++ src/components/LoginForm1.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="card-front">
     <div class="center-wrap">
-      <Form @submit="login" class="section text-center">
+      <Form @submit="login" class="section text-center" :validation-schema="loginFormSchema">
         <h4 class="mb-4 pb-3">ورود</h4>
         <div class="form-group">
-          <!-- v-model="email" -->
           <Field
-            :rules="validateEmail"
+            :validateOnInput="true"
             name="email"
             type="email"
             class="form-style"
@@ -19,9 +18,8 @@
             <ErrorMessage name="email" />
         </p>
         <div class="form-group mt-2">
-          <!-- v-model="password" -->
           <Field
-            :rules="validatePassword"
+            :validateOnInput="true"
             name="password"
             type="password"
             class="form-style"
@@ -44,6 +42,8 @@
 
 <script>
 import { Form, Field, ErrorMessage} from "vee-validate";
+import * as yup from 'yup';
+
 export default {
   name: "LoginForm",
   components:{
@@ -52,34 +52,44 @@ export default {
     ErrorMessage,
   },
   data(){
+    // validate with yup schema
+    const loginFormSchema = yup.object({
+      email : yup.string().required("ایمیل را وارد کنید").email("ایمیل نامعتبر است"),
+      password : yup.string().required("لطفا رمز عبور را وارد کنید").min(5,"لطفا رمز عبور با بیش از 5 کاراکتر انتخاب کنید")
+    })
     return{
-      email:"",
-      password:"",
+      // validate with yup
+      // emailRule:yup.string().required("ایمیل را وارد کنید").email("ایمیل نامعتبر است"),
+      // passwordRule:yup.string().required("لطفا رمز عبور را وارد کنید").min(5,"لطفا رمز عبور با بیش از 5 کاراکتر انتخاب کنید")
+      
+      loginFormSchema
     }
   },
   methods:{
     login(values){
       console.log(values)
     },
-    validateEmail(email) {
-      if (!email) {
-        return "لطفا ایمیل را وارد کنید"
-      }
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-      if (!regex.test(email)) {
-        return 'ایمیل نا معتبر است';
-      }
-      return true;
-    },
-    validatePassword(password){
-      if (!password) {
-        return "لطفا رمز عبور را وارد کنید"
-      }
-      if (password.length <= 5) {
-        return "لطفا رمز عبور با بیش از 5 کاراکتر انتخاب کنید"
-      }
-      return true;
-    }
+    // old way
+    // validateEmail(email) {
+    //   if (!email) {
+    //     return "لطفا ایمیل را وارد کنید"
+    //   }
+    //   const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    //   if (!regex.test(email)) {
+    //     return 'ایمیل نا معتبر است';
+    //   }
+    //   return true;
+    // },
+    // validatePassword(password){
+    //   if (!password) {
+    //     return "لطفا رمز عبور را وارد کنید"
+    //   }
+    //   if (password.length <= 5) {
+    //     return "لطفا رمز عبور با بیش از 5 کاراکتر انتخاب کنید"
+    //   }
+      
+    //   return true;
+    // }
 
   }
 };
